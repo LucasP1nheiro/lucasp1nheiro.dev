7d6011a27063dd584137ce622d32d190d78523ac
Lucas P1nheiro <lps1704@gmail.com>
2025-02-06 18:40:37 -0300

    style: changes marker text color to text-accent-foreground

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -41,7 +41,7 @@ const ProjectPage = async ({
         priority
       />
       <section
-        className={`prose prose-li:text-muted-foreground prose-p:text-muted-foreground prose-h1:text-accent-foreground prose-strong:text-accent-foreground prose-h2:text-accent-foreground prose-h3:text-accent-foreground prose-a:text-accent-foreground`}
+        className={`prose prose-li:text-muted-foreground marker:text-accent-foreground prose-p:text-muted-foreground prose-h1:text-accent-foreground prose-strong:text-accent-foreground prose-h2:text-accent-foreground prose-h3:text-accent-foreground prose-a:text-accent-foreground`}
       >
         {project.markdown}
       </section>
