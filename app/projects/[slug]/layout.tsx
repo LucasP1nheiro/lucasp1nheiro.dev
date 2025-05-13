6e94ac8cee177115d669613bb24f5d5fec499969
Lucas P1nheiro <lps1704@gmail.com>
2025-05-13 14:32:49 -0300

    feat: adds theme-toggle to project layout

--- app/projects/[slug]/layout.tsx
+++ app/projects/[slug]/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from '@/components/theme-toggle';
 import { Button } from '@/components/ui/button';
 import Container from '@/components/ui/container';
 import { ArrowLeft } from 'lucide-react';
@@ -11,13 +12,14 @@ export default function ProjectLayout({
   return (
     <Container>
       <div className="w-full md:py-24 py-8">
-        <header className="mb-12">
+        <header className="mb-12 flex items-center justify-between">
           <Button asChild variant={'ghost'}>
             <Link href={'/'}>
               <ArrowLeft />
               <p>Back to home</p>
             </Link>
           </Button>
+          <ThemeToggle />
         </header>
         {children}
       </div>
