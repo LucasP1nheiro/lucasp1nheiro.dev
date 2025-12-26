a1315ae4ee4eb0a8862af5454f84814e069fcde2
Lucas P1nheiro <lps1704@gmail.com>
2025-12-26 15:57:21 -0300

    refactor: changes location's section text

--- components/geo.tsx
+++ components/geo.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import UTC8Clock from '@/components/utc-8-clock';
+import UTCMinus3Clock from '@/components/utc-minus-3-clock';
 import Globe from './ui/globe';
-
 
 const Geo = () => {
   return (
-    <div className="flex flex-col justify-between items-center py-16">
-      <UTC8Clock />
+    <div className="flex flex-col justify-between items-center py-4">
+      <UTCMinus3Clock />
       <Globe />
       <div className="text-center">
-        <p className="max-w-[500px]">
-          Not sure where <strong>Bragança Paulista</strong> is? Here’s a quick
-          heads-up—it’s my home! :)
+        <p className="max-w-[500px] text-muted-foreground">
+          I operate in the <strong>UTC-3 timezone</strong> (Brasília Time),
+          located in Bragança Paulista, São Paulo, Brazil.
         </p>
       </div>
     </div>
